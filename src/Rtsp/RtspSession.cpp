55dcae06a08cdefa506d0472d788d66d98962986
Ikko Ashimine <eltociear@gmail.com>
2022-12-14 07:48:26 +0000

    fix typo in RtspSession.cpp availabe -> available
    Signed-off-by: Ikko Ashimine <eltociear@gmail.com>

--- src/Rtsp/RtspSession.cpp
+++ src/Rtsp/RtspSession.cpp
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Copyright (c) 2016 The ZLMediaKit project authors. All Rights Reserved.
  *
  * This file is part of ZLMediaKit(https://github.com/xia-chu/ZLMediaKit).
@@ -307,7 +307,7 @@ void RtspSession::handleReq_ANNOUNCE(const Parser &parser) {
 void RtspSession::handleReq_RECORD(const Parser &parser){
     if (_sdp_track.empty() || parser["Session"] != _sessionid) {
         send_SessionNotFound();
-        throw SockException(Err_shutdown, _sdp_track.empty() ? "can not find any availabe track when record" : "session not found when record");
+        throw SockException(Err_shutdown, _sdp_track.empty() ? "can not find any available track when record" : "session not found when record");
     }
 
     _StrPrinter rtp_info;
